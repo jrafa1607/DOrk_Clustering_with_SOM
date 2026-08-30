3cbb48ff3f4ea97f56ec70df95a2d940652186ba
João Rafael <107967743+jrafa1607@users.noreply.github.com>
2026-08-30 15:09:49 -0300

    Add files via upload

---
+++ QuantizationError/ErroCalc.py
@@ -0,0 +1,24 @@
+'Calc for High QuantizationError Nodes (SOM)'
+Total = 100;
+Size = 225;
+Nodes = 13;
+
+Result = Total * Nodes / Size
+print(Result);
+
+'''
+Result Map 01 - 11.1% (25 Node)
+Result Map 02 - 00.0% (0 Nodes)
+Result Map 03 - 17.3% (39 Nodes)
+Result Map 04 - 09.7% (22 Nodes)
+Result Map 05 - 10.2% (23 Nodes)
+Result Map 06 - 18.2% (41 Nodes)
+Result Map 07 - 20.8% (47 Nodes)
+Result Map 08 - 33.3% (75 Nodes)
+Result Map 09 - 19.5% (44 Nodes)
+Result Map 10 - 00.0% (0 Nodes)
+Result Map 11 - 09.7% (22 Nodes)
+Result Map 12 - 24.8% (56 Nodes)
+Result Map 13 - 25.3% (57 Nodes)
+Result Map 14 - 05.7% (13 Nodes)
+'''
